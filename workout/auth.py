7ceed2843860d2f7680aca49751e071c2c92713e
mksahota10 <mksahota10@gmail.com>
2021-02-18 23:40:04 -0800

    added backend data

--- workout/auth.py
+++ workout/auth.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
-
 from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login')
 def login():
